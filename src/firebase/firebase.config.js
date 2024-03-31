273457f8fcbc467beff00e567589127dd015f7d0
Md Abdul Kader <aknkhan2000@gmail.com>
2024-04-01 02:41:53 +0600

    project is completed

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC2IaW3PdMjuv943cgDDUK_do9zIbsvxqA",
-  authDomain: "dragon-news-portal-react.firebaseapp.com",
-  projectId: "dragon-news-portal-react",
-  storageBucket: "dragon-news-portal-react.appspot.com",
-  messagingSenderId: "227072933394",
-  appId: "1:227072933394:web:fbf80a4fa4ab521b96e2cb"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
